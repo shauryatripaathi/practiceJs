dc22ae69560e1cf916f0d6af9485595f2f3d3a50
Shaurya Tripathi <shauryatripaathi@gmail.com>
2023-12-13 14:36:25 +0530

    background with changing color

--- Practice/init.js
+++ Practice/init.js
@@ -4,3 +4,11 @@ btn.id = 'buttonId';
 btn.innerHTML = 'click here';
 container.append(btn);
 
+function handleOnBtn(){
+    let x = Math.trunc(100 * Math.random());
+    let y = Math.trunc(100 * Math.random());
+    let z = Math.trunc(100 * Math.random());
+    document.body.style.backgroundColor = `rgb(${x},${y},${z})`;
+    console.log(x,y,z);
+}
+btn.addEventListener("click",handleOnBtn);
