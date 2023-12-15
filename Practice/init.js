33642bc4c6359c380d14210aae8a33556d7ff698
Shaurya Tripathi <shauryatripaathi@gmail.com>
2023-12-16 00:53:01 +0530

    update init.js

--- Practice/init.js
+++ Practice/init.js
@@ -4,7 +4,25 @@ btn.id = 'buttonId';
 btn.innerHTML = 'click here';
 container.append(btn);
 
+let navBar = document.createElement("nav");
+navBar.id = "navBar";
+navBar.innerHTML = 'hello';
+container.append(navBar);
+
+
+let c = document.getElementById('myCanvas');
+const ctx = c.getContext("2d")
+ctx.beginPath();
+ctx.arc(200,65,50,0,2*Math.PI);
+ctx.stroke();
+ctx.lineWidth = 5;
+ctx.fillStyle = 'orange';
+ctx.fill();
+ctx.strokeStyle = 'white';
+ctx.stroke();
+
 function handleOnBtn(){
+    
     let x = Math.trunc(100 * Math.random());
     let y = Math.trunc(100 * Math.random());
     let z = Math.trunc(100 * Math.random());
@@ -12,3 +30,5 @@ function handleOnBtn(){
     console.log(x,y,z);
 }
 btn.addEventListener("click",handleOnBtn);
+
+
