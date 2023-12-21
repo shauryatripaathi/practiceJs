257aab50bc07d495cb237baa4207c217efbbd2a5
Shaurya Tripathi <shauryatripaathi@gmail.com>
2023-12-21 14:34:42 +0530

    tic tac toe elements added

--- Practice/init.js
+++ Practice/init.js
@@ -71,28 +71,72 @@ container.append(box);
 let tile1 = document.createElement("div");
 tile1.id = "tile1";
 box.append(tile1);
+tile1.innerText = "1";
 let tile2 = document.createElement("div");
 tile2.id = "tile2";
+tile2.innerText = "2";
 box.append(tile2);
 let tile3 = document.createElement("div");
 tile3.id = "tile3";
+tile3.innerText = "3";
 box.append(tile3);
 let tile4 = document.createElement("div");
 tile4.id = "tile4";
+tile4.innerText = "4";
 box.append(tile4);
 let tile5 = document.createElement("div");
 tile5.id = "tile5";
+tile5.innerText = "5";
 box.append(tile5);
 let tile6 = document.createElement("div");
 tile6.id = "tile6";
+tile6.innerText = "6";
 box.append(tile6);
 let tile7 = document.createElement("div");
 tile7.id = "tile7";
+tile7.innerText = "7";
 box.append(tile7);
 let tile8 = document.createElement("div");
 tile8.id = "tile8";
+tile8.innerText = "8";
 box.append(tile8);
 let tile9 = document.createElement("div");
 tile9.id = "tile9";
+tile9.innerText = "9";
 box.append(tile9);
+
+let n = 1;
+let moveX = 'x';
+let moveO = 'o'
+let img = document.createElement('img');
+img.style.width = '50px';
+img.style.height = '50px';
+img.src = './panda.jpg';
+
+function handleOnBox(e){
+    alert(e.target.id);
+    if(e.target.innerText == 'x'){
+        alert('Choose different tile');
+    }
+    else if(e.target.innerText == 'o'){
+        alert('Choose different tile');
+    }
+    else if(e.target.id == 'box'){
+        alert('out of bounds');
+    }
+    else if(n%2 == 1){
+        e.target.innerText = moveX;
+        alert(e.target.innerText);
+        console.log(e.target.innerText)
+        tile1.append(img);
+        n = n+1;
+    }
+    else if(n%2 == 0){
+        e.target.innerText = moveO;
+        alert(e.target.innerText);
+        console.log(e.target.innerText)
+        n = n+1;
+     }
+}
+box.addEventListener("click",handleOnBox);
 
